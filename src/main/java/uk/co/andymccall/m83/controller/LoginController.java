735a8cdd907f6893cdf42fdbd28ec8b68ff642c5
Andy McCall <mailme@andymccall.co.uk>
2017-04-19 22:44:44 +0100

    Added tooltips Added login attempts Added failed login message

--- src/main/java/uk/co/andymccall/m83/controller/LoginController.java
+++ src/main/java/uk/co/andymccall/m83/controller/LoginController.java
@@ -3,15 +3,20 @@ package uk.co.andymccall.m83.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.SequentialTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginController implements Initializable{
+public class LoginController implements Initializable {
 
     @FXML
     private JFXTextField usernameTextField;
@@ -26,15 +31,43 @@ public class LoginController implements Initializable{
     private JFXButton cancelButton;
 
     @FXML
+    private Label messageLabel;
+
+    private int loginAttempts;
+
+    @FXML
     public void initiateLogin(ActionEvent event) {
+
         String username = usernameTextField.getText();
         String password = passwordPasswordField.getText();
 
-        System.out.println("Username: " + username + " Password: " + password);
+        setLoginAttempts(getLoginAttempts() + 1);
+
+        if (username.contentEquals("username") & (password.contentEquals("password"))) {
+            System.out.println("Username: " + username + " Password: " + password);
+            System.out.println("Login correct!");
+        } else {
+
+            if (getLoginAttempts() >= 3) {
+                System.out.println("Account locked out");
+            }
+
+            getMessageLabel().setText("Incorrect username or password");
+            getMessageLabel().setVisible(true);
+
+            FadeTransition fade = new FadeTransition(Duration.seconds(3), getMessageLabel());
+            fade.setFromValue(1);
+            fade.setToValue(0);
+            SequentialTransition fadeMessage = new SequentialTransition(fade);
+            fadeMessage.play();
+            fade.setOnFinished(fadeEvent -> getMessageLabel().setVisible(false));
+
+        }
+
     }
 
     @FXML
-    public void calncelLogin(ActionEvent event) {
+    public void cancelLogin(ActionEvent event) {
 
         System.out.println("Exiting M83");
 
@@ -74,7 +107,27 @@ public class LoginController implements Initializable{
         this.cancelButton = cancelButton;
     }
 
+    public Label getMessageLabel() {
+        return messageLabel;
+    }
+
+    public void setMessageLabel(Label messageLabel) {
+        this.messageLabel = messageLabel;
+    }
+
+    public int getLoginAttempts() {
+        return loginAttempts;
+    }
+
+    public void setLoginAttempts(int loginAttempts) {
+        this.loginAttempts = loginAttempts;
+    }
+
     public void initialize(URL url, ResourceBundle rb) {
+
+        getMessageLabel().setVisible(false);
+        getLoginButton().setDefaultButton(true);
+        setLoginAttempts(0);
 
     }
 
